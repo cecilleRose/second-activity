da59569d5daedeb9fe9c2420bb3ff479861876c0
SagravJerry <SagravJerry>
2022-03-29 17:11:27 +0800

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public actionSheetCtrl:ActionSheetController) {}
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'contact',
+      cssClass: 'custom-css',
+      animated: true,
+      backdropDismiss: true,
+      keyboardClose: false,
+      mode: 'ios',
+      buttons: [
+        {
+          text:'Call',
+          icon: 'call-outline',
+          handler: ()=> {
+            console.log('the call button has been clicked');
+          }
+          
+        },
+        {
+          text:'Send Message',
+          role:'cancel',
+          icon: 'mail-outline',
+          handler: ()=> {
+            console.log('the message button has been clicked');
+          }
+        },
+        {
+          text:'Cancel',
+          role:'destructive',
+          icon: 'close',
+          handler: ()=> {
+            console.log('the cancel button has been clicked');
+          }
+        }
+      ]
+     
 
+    });
+    actionSheet.present();
+  }
 }
